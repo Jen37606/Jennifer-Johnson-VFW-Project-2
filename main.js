47be65d1f7e83f62bae93bc4922be5ff9340db48
Jen <jen37606@yahoo.com>
2011-08-12 16:33:09 -0400

    Finally got it working! Added the other form data :)

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-window.onload = saveItems();
-
 function getItems(){
 	if(localStorage.getItem('apptitle')){
 		var genre = localStorage.getItem('appgenre');
@@ -8,12 +6,24 @@ function getItems(){
 		var director = localStorage.getItem('appdirector');
 		var rating = localStorage.getItem('apprating');
 		var favorites = localStorage.getItem('appfavorites');
+		var family = localStorage.getItem('appfamily');
 		var release = localStorage.getItem('apprelease');
+		var description = localStorage.getItem('appdescription');
 		
-		var viewMovie = [genre,	title, actor, director, rating, favorites, release];
+		var viewMovie = [genre,	title, actor, director, rating, favorites, family, release, description];
 		
 		console.log(viewMovie);
 		alert(viewMovie);
+		document.getElementById('main').style.display = "none";
+		var clearLink = document.getElementById('clear');
+		clearLink.style.display = "block";
+	}else{
+		var title = "Enter Movie Title";
+		var actor = "Enter Actor/Actress Name";
+		var director = "Enter Director Name";
+		var title = document.getElementById('title').value = title;
+		var actor = document.getElementById('actor').value = actor;
+		var director = document.getElementById('director').value = director;
 	}
 }
 
@@ -24,13 +34,25 @@ function saveItems(id){
 	var director = document.getElementById('director').value;
 	var rating = document.getElementById('rating').value;
 	var favorites = document.getElementById('favorites').value;
+	if(document.getElementById('yes').checked){
+		var family = "family movie"
+	}else{
+		var family = "not a family movie"
+	}
 	var release = document.getElementById('release').value;
+	var description = document.getElementById('description').value;
 	localStorage.setItem('appgenre', genre);
 	localStorage.setItem('apptitle', title);
 	localStorage.setItem('appactor', actor);
 	localStorage.setItem('appdirector', director);
 	localStorage.setItem('apprating', rating);
 	localStorage.setItem('appfavorites', favorites);
+	localStorage.setItem('appfamily', family);
 	localStorage.setItem('apprelease', release);
+	localStorage.setItem('appdescription', description);
 }
 
+function clearItems(){
+	localStorage.clear();
+	return false;
+}
