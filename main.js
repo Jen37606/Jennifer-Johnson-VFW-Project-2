6c9302b82e0af66c4c56ffbc28fe2351e638f897
Jen <jen37606@yahoo.com>
2011-08-11 23:53:58 -0400

    Trying to get it to work...

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-window.onload = saveItems;
+window.onload = saveItems();
 
-var getItems = function(){
+function getItems(){
 	if(localStorage.getItem('apptitle')){
 		var genre = localStorage.getItem('appgenre');
 		var title = localStorage.getItem('apptitle');
@@ -8,37 +8,22 @@ var getItems = function(){
 		var director = localStorage.getItem('appdirector');
 		var rating = localStorage.getItem('apprating');
 		var favorites = localStorage.getItem('appfavorites');
-		var family = localStorage.getItem('appfamily');
 		var release = localStorage.getItem('apprelease');
 		
-		var viewMovie = [
-			genre,
-			title,
-			actor,
-			director,
-			rating,
-			favorites,
-			family,
-			release
-		];
+		var viewMovie = [genre,	title, actor, director, rating, favorites, release];
 		
 		console.log(viewMovie);
 		alert(viewMovie);
 	}
-};
+}
 
-var saveItems = function(id){
+function saveItems(id){
 	var genre = document.getElementById('genre').value;
 	var title = document.getElementById('title').value;
 	var actor = document.getElementById('actor').value;
 	var director = document.getElementById('director').value;
 	var rating = document.getElementById('rating').value;
 	var favorites = document.getElementById('favorites').value;
-	var family = document.getElementById('family').value;
-		if(family == null){
-			location.reload(true);
-			localStorage.setItem('family',0);
-		}
 	var release = document.getElementById('release').value;
 	localStorage.setItem('appgenre', genre);
 	localStorage.setItem('apptitle', title);
@@ -46,7 +31,6 @@ var saveItems = function(id){
 	localStorage.setItem('appdirector', director);
 	localStorage.setItem('apprating', rating);
 	localStorage.setItem('appfavorites', favorites);
-	localStorage.setItem('appfamily', family);
 	localStorage.setItem('apprelease', release);
-};
+}
 
